da3e69aea68f542ceaf873c3a73cea962abbea14
Rickson Rocha <ricksonroccha21@gmail.com>
2025-03-29 20:03:20 -0300

    [fix]: fixes assignment error for email attribute

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,46 +1,52 @@
-import { randomUUID } from "crypto";
+import { randomUUID } from 'crypto';
 
 interface UserParams {
-    name: string;
-    password: string;
-    createdAt?: Date;
+  name: string;
+  email: string;
+  password: string;
+  createdAt?: Date;
 }
 export class UserEntity {
-    private id: string;
-    private name: string;
-    private password: string;
-    private createdAt: Date;
+  private id: string;
+  private name: string;
+  private email: string;
+  private password: string;
+  private createdAt: Date;
 
-    constructor(params: UserParams, id?: string) {
-        this.id = id || randomUUID();
-        this.name = params.name;
-        this.password = params.password;
-        this.createdAt = params.createdAt ?? new Date();
-    }
+  constructor(params: UserParams, id?: string) {
+    this.id = id || randomUUID();
+    this.name = params.name;
+    this.password = params.password;
+    this.email = params.email;
+    this.createdAt = params.createdAt ?? new Date();
+  }
 
-    public getId(): string {
-        return this.id;
-    }
+  public getId(): string {
+    return this.id;
+  }
 
-    public getName(): string {
-        return this.name;
-    }
+  public getName(): string {
+    return this.name;
+  }
 
-    public setName(name: string): void {
-        this.name = name;
-    }
+  public setName(name: string): void {
+    this.name = name;
+  }
 
-    public getPassword(): string {
-        return this.password;
-    }
+  public getPassword(): string {
+    return this.password;
+  }
 
-    public setPassword(password: string): void {
-        this.password = password;
-    }
-
-    public getCreatedAt(): Date {
-        return this.createdAt;
-    }
-
-
+  public setPassword(password: string): void {
+    this.password = password;
+  }
+  public getEmail(): string {
+    return this.email;
+  }
+  public setEmail(email: string): void {
+    this.email = email;
+  }
+  public getCreatedAt(): Date {
+    return this.createdAt;
+  }
 }
