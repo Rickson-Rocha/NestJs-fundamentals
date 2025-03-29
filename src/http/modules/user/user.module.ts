a15b70bebdca1168d3705fad3a9c5f584a9dfd9c
Rickson Rocha <ricksonroccha21@gmail.com>
2025-03-29 20:00:57 -0300

    [feat]:create controller,module,dto and viewModel to entity user

---
+++ src/http/modules/user/user.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { UserController } from './user.controller';
+import { DatabaseModule } from 'src/infra/database/database.module';
+import { CreateUserUseCase } from 'src/modules/user/usecases/create.user.use.case';
+
+@Module({
+  imports: [DatabaseModule],
+  controllers: [UserController],
+  providers: [CreateUserUseCase],
+})
+export class UserModule { }
