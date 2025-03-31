2427c41419074b8018d5a76f8f721224a7353ac0
Rickson Rocha <ricksonroccha21@gmail.com>
2025-03-31 08:57:38 -0300

    [fix]:change directory to htpp modules

---
+++ src/infra/http/modules/user/user.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserUseCase } from 'src/modules/user/usecases/create.user.use.case';
+import { CreateUserDto } from './dtos/create.user.dto';
+import { UserViewModel } from './viewModel/user.view.model';
+
+@Controller('users')
+export class UserController {
+    constructor(private createUserUserCase: CreateUserUseCase) { }
+    @Post()
+    async create(@Body() data: CreateUserDto) {
+        const { name, email, password } = data;
+        const user = await this.createUserUserCase.execute({
+            name,
+            email,
+            password,
+        });
+        console.log("user do controller" + user)
+        return UserViewModel.toHttpResponse(user);
+    }
+}
