2cd87f4cf61f2c4c65d28a37f45f6f005cad1086
Rickson Rocha <ricksonroccha21@gmail.com>
2025-04-01 15:10:21 -0300

    [style]:apply lint

--- src/modules/task/entities/task.entity.ts
+++ src/modules/task/entities/task.entity.ts
@@ -1,50 +1,49 @@
-import { randomUUID } from "crypto";
+import { randomUUID } from 'crypto';
 
 export interface TaskParams {
-    title: string;
-    description?: string;
-    userId: string;
-    createdAt: Date;
+  title: string;
+  description?: string;
+  userId: string;
+  createdAt?: Date;
 }
 export class TaskEntity {
-    private id: string
-    private title: string
-    private description: string
-    private userId: string
-    private createdAt: Date
+  private id: string;
+  private title: string;
+  private description: string;
+  private userId: string;
+  private createdAt: Date;
 
-    constructor(params: TaskParams, id?: string) {
-        this.id = id || randomUUID();
-        this.title = params.title;
-        this.description = params.description ?? null
-        this.userId = params.userId
-        this.createdAt = params.createdAt ?? new Date();
-    }
-    public getId(): string {
-        return this.id;
-    }
+  constructor(params: TaskParams, id?: string) {
+    this.id = id || randomUUID();
+    this.title = params.title;
+    this.description = params.description ?? null;
+    this.userId = params.userId;
+    this.createdAt = params.createdAt ?? new Date();
+  }
+  public getId(): string {
+    return this.id;
+  }
 
-    public getTitle(): string {
-        return this.title;
-    }
+  public getTitle(): string {
+    return this.title;
+  }
 
-    public setTitle(title: string): void {
-        this.title = title;
-    }
+  public setTitle(title: string): void {
+    this.title = title;
+  }
 
-    public getDescription(): string | null {
-        return this.description;
-    }
+  public getDescription(): string | null {
+    return this.description;
+  }
 
-    public setDescription(description: string | null): void {
-        this.description = description;
-    }
+  public setDescription(description: string | null): void {
+    this.description = description;
+  }
 
-    public getUserId(): string {
-        return this.userId;
-    }
-    public getCreatedAt(): Date {
-        return this.createdAt;
-    }
-
+  public getUserId(): string {
+    return this.userId;
+  }
+  public getCreatedAt(): Date {
+    return this.createdAt;
+  }
 }
