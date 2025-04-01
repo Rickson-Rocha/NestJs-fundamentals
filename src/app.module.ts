71ec35153222997d89a798e44cd06cedd46259ef
Rickson Rocha <ricksonroccha21@gmail.com>
2025-04-01 10:47:09 -0300

    [feat]: add providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './http/modules/user/user.module';
+import { UserModule } from './infra/http/modules/user/user.module';
 import { DatabaseModule } from './infra/database/database.module';
+import { AuthModule } from './infra/http/modules/auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './infra/http/modules/auth/guards/jwt.auth.guard';
 
 @Module({
-  imports: [DatabaseModule, UserModule],
+  imports: [DatabaseModule, UserModule, AuthModule],
   controllers: [],
-  providers: [],
+  providers: [{
+    provide: APP_GUARD,
+    useClass: JwtAuthGuard,
+  },],
 })
-export class AppModule {}
+export class AppModule { }
